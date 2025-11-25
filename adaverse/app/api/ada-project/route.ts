d588bed6c897cf54fd359c4c164c59b9077a5ad2
Alexis SANTOS <alexisamsantos@gmail.com>
2025-11-25 10:53:23 +0100

    Revert "Database pre-configuration completed"
    This reverts commit 260d9db11bec626d74a66cc19ac028c91f28dc5f.

--- adaverse/app/api/ada-project/route.ts
+++ adaverse/app/api/ada-project/route.ts
@@ -1,7 +1,7 @@
-import db from "@/src/lib/db";
-import {adaProjects} from "@/src/lib/db/schema";
+import db from "@/lib/db";
+import {adaProjects} from "@/lib/db/schema";
 import { NextResponse } from "next/server";
-import { normalizeText } from "@/src/utils/normalizeText";
+import { normalizeText } from "@/utils/normalizeText";
 
 // GET /api/ada-project - Fetch all projects
 export async function GET() {
